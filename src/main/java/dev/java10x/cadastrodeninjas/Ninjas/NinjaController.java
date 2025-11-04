cb707294c306dd09d63c513a11d53bd68abb41ab
Victor Teruya <victorhteruya@gmail.com>
2025-11-04 17:09:02 -0300

    fix: Altera o nome das rotas do controller dos ninjas

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package dev.java10x.cadastrodeninjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
