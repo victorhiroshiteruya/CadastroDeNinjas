d5d0289482aa9e345619f7ceadf73a85c72a0173
Victor Teruya <victorhteruya@gmail.com>
2025-11-04 17:07:55 -0300

    feat: Cria as rotas Controller com mock

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesController.java
@@ -1,4 +1,33 @@
 package dev.java10x.cadastrodeninjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    // GET -- Manda requisição para listar as missões
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Lista de missoes";
+    }
+
+    // POST -- Manda requisição para criar uma nova missão
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada com sucesso";
+    }
+
+    // PUT -- Manda requisição para alterar uma missão
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missao alterada com sucesso";
+    }
+
+    // DELETE -- Manda requisição para deletar uma missão
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missao deletada com sucesso";
+    }
+
 }
