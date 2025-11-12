96508257a629b3b6b11b5f089cda2081a77839a7
Victor Teruya <victorhteruya@gmail.com>
2025-11-12 20:16:57 -0300

    feat: Adiciona DTO e Mapper para Missoes

---
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/MissoesMapper.java
@@ -0,0 +1,26 @@
+package dev.java10x.cadastrodeninjas.Missoes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setNinja(missoesDTO.getNinja());
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel) {
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setNinja(missoesDTO.getNinja());
+        return missoesDTO;
+    }
+
+}
